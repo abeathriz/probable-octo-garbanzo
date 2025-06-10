d59aedd0ae14800d500448716deec1aeb57a9279
Ana Beathriz Santos <150723235+abeathriz@users.noreply.github.com>
2025-06-10 01:16:15 -0300

    Update AP2Streamlit.py

--- codigos/AP2Streamlit.py
+++ codigos/AP2Streamlit.py
@@ -1,16 +1,16 @@
-df = pd.read_csv('../bases_tratadas/dadostratadosfarfetch.csv', sep=';')
+df = pandas.read_csv('../bases_tratadas/dadostratadosfarfetch.csv', sep=';')
 
-st.dataframe(df)
+streamlit.dataframe(df)
 
-st.subheader('Análise de nulos')
+streamlit.subheader('Análise de nulos')
 aux = df.isnull().sum().reset_index()
 aux.columns = ['preco', 'desconto']
-st.dataframe(aux)
+streamlit.dataframe(aux)
 
 
-st.subheader('Análises univariadas')
-st.write('Medidas resumo')
-st.dataframe(df.describe())
+streamlit.subheader('Análises univariadas')
+streamlit.write('Medidas resumo')
+streamlit.dataframe(df.describe())
 
 #SELECIONAR COLUNA
 lista_de_colunas = df.columns
@@ -22,7 +22,7 @@ colunas_numericas = df.select_dtypes(include='number').columns.tolist()
 if colunas_numericas:
 
     # SELECIONA
-    coluna_escolhida = st.selectbox('Escolha a coluna', colunas_numericas)
+    coluna_escolhida = streamlit.selectbox('Escolha a coluna', colunas_numericas)
 
     media = round(df[coluna_escolhida].mean(),2)
     desvio = round(df[coluna_escolhida].std(),2)
@@ -30,12 +30,12 @@ if colunas_numericas:
     maximo = round(df[coluna_escolhida].max(),2)
     minimo = round(df[coluna_escolhida].max(),2)
 
-    st.write(f"**Média**: {media}")
-    st.write(f"**Desvio padrão**: {desvio}")
-    st.write(f"**Mediana**: {mediana}")
-    st.write(f"**Máximo**: {maximo}")
+    streamlit.write(f"**Média**: {media}")
+    streamlit.write(f"**Desvio padrão**: {desvio}")
+    streamlit.write(f"**Mediana**: {mediana}")
+    streamlit.write(f"**Máximo**: {maximo}")
 else:
-    st.warning("Não contém coluna numérica.")
+    streamlit.warning("Não contém coluna numérica.")
 
 print('Média de valores descontados:', media)
 print('Desvio padrão de valores descontados:', desvio)
@@ -43,25 +43,25 @@ print('Mediana dos valores descontados:', mediana)
 print('Máximo dos valores descontados:' , maximo)
 print('Menor valor dos descontados:', minimo)
 
-st.write(f'A coluna escolhida foi {coluna_escolhida}. A sua média é {media}. Seu desvio padrão indica que, quando há desvio, desvia em média {desvio}. E 50% dos dados vão até o valor {mediana}. E seu máximo é de {maximo}.')
-st.write(f'O produto com o maior valor, custa {minimo} reais')
-st.write('Histograma')
-fig = px.histogram(df,x=[coluna_escolhida])
-st.plotly_chart(fig)
-st.write('Boxplot')
-fig2 = px.box(df, x=[coluna_escolhida])
-st.plotly_chart(fig2)
+streamlit.write(f'A coluna escolhida foi {coluna_escolhida}. A sua média é {media}. Seu desvio padrão indica que, quando há desvio, desvia em média {desvio}. E 50% dos dados vão até o valor {mediana}. E seu máximo é de {maximo}.')
+streamlit.write(f'O produto com o maior valor, custa {minimo} reais')
+streamlit.write('Histograma')
+fig = plotly.express.histogram(df,x=[coluna_escolhida])
+streamlit.plotly_chart(fig)
+streamlit.write('Boxplot')
+fig2 = plotly.express.box(df, x=[coluna_escolhida])
+streamlit.plotly_chart(fig2)
 
-st.subheader('Análises multivariadas')
-lista_de_escolhas = st.multiselect('Escolha mais de uma coluna para avaliar', lista_de_colunas)
-st.markdown('Gráfico de dispersão')
+streamlit.subheader('Análises multivariadas')
+lista_de_escolhas = streamlit.multiselect('Escolha mais de uma coluna para avaliar', lista_de_colunas)
+streamlit.markdown('Gráfico de dispersão')
 if len(lista_de_escolhas)>2 or len(lista_de_escolhas)<2:
-    st.error('Escolha somente 2 colunas')
+    streamlit.error('Escolha somente 2 colunas')
 else:
-    fig3 = px.scatter(df, x=lista_de_escolhas[0], y=lista_de_escolhas[1])
-    st.plotly_chart(fig3)
-    st.markdown('Gráfico de caixa')
-    fig4 = px.box(df, x=lista_de_escolhas[0], y=lista_de_escolhas[1])
-    st.plotly_chart(fig4)
-    fig5 = px.pie(df, lista_de_escolhas[0], lista_de_escolhas[1])
-    st.plotly_chart(fig5)
+    fig3 = plotly.express.scatter(df, x=lista_de_escolhas[0], y=lista_de_escolhas[1])
+    streamlit.plotly_chart(fig3)
+    streamlit.markdown('Gráfico de caixa')
+    fig4 = plotly.express.box(df, x=lista_de_escolhas[0], y=lista_de_escolhas[1])
+    streamlit.plotly_chart(fig4)
+    fig5 = plotly.express.pie(df, lista_de_escolhas[0], lista_de_escolhas[1])
+    streamlit.plotly_chart(fig5)
